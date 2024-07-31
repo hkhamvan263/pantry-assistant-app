9ac1af84ccb3298dca2dad10e0cca8fdc1d24daf
Hannah Khamvan <92757465+hkhamvan263@users.noreply.github.com>
2024-07-31 15:41:44 +0000

    Updated page.js and the outer README file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pantry-assistant-app
+
+The Pantry Assistant app is an app where you add an item.
--- pantry-assistant-app/app/page.js
+++ pantry-assistant-app/app/page.js
@@ -2,7 +2,15 @@
 import {Box, Modal, Stack, TextField, Typography, Button} from '@mui/material'
 import {firestore} from '@/firebase'
 import {useState, useEffect} from 'react'
-import {collection, doc, deleteDoc, getDocs, query, setDoc, getDoc} from 'firebase/firestore'
+import {
+  collection,
+  doc,
+  deleteDoc,
+  getDocs,
+  query,
+  setDoc,
+  getDoc
+} from 'firebase/firestore'
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
@@ -47,8 +55,6 @@ export default function Home() {
       } else {
         await setDoc(docRef, {quantity: quantity - 1})
       }
-    } else {
-      console.log("Cannot remove a nonexistent item.")
     }
 
     await updateInventory()
@@ -109,6 +115,8 @@ export default function Home() {
           </Stack>
         </Box>
       </Modal>
+      <Typography variant='h1' color='#333' textAlign='center'>Pantry Assistant</Typography>
+      <Typography color='#333'>Hello and this is the Pantry Assistant.</Typography>
       <Button
         variant="contained"
         onClick={() => {
@@ -130,23 +138,46 @@ export default function Home() {
             Pantry Items
           </Typography>
         </Box>
+        <Stack width="800px" height="300px" spacing={2} overflow="auto">
+          {inventory.map(({name, quantity}) => (
+            <Box
+              key={name}
+              width="100%"
+              minHeight="150px"
+              display='flex'
+              alignItems="center"
+              justifyContent="space-between"
+              bgcolor='f0f0f0'
+              padding={5}
+            >
+              <Typography variant='h3' color='#333' textAlign='center'>
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Typography variant='h3' color='#333' textAlign='center'>
+                {quantity}
+              </Typography>
+              <Stack direction="row" spacing={2}>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    addItem(name)
+                  }}
+                >
+                  Add
+                </Button>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    removeItem(name)
+                  }}
+                >
+                  Remove
+                </Button>
+              </Stack>
+            </Box>
+          ))}
+        </Stack>
       </Box>
-      <Stack width="800px" height="300px" spacing={2} overflow="auto">
-        {inventory.map(({name, quantity}) => (
-          <Box
-            key={name}
-            width="100%"
-            minHeight="150px"
-            display='flex'
-            alignItems="center"
-            justifyContent="center"
-            bgcolor='f0f0f0'
-            padding={5}
-          >
-            <Typography>Name</Typography>
-          </Box>
-        ))}
-      </Stack>
     </Box>
   )
 }

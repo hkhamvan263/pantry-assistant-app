98e847eff552645a59cd64ce0f510ec21f15f6e5
Hannah Khamvan <92757465+hkhamvan263@users.noreply.github.com>
2024-07-31 13:41:36 +0000

    Initial commit

---
+++ pantry-assistant-app/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRYDk1HgetR7bY1_gUcJ8ZMLtLcXAn0uQ",
+  authDomain: "pantry-assistant-app.firebaseapp.com",
+  projectId: "pantry-assistant-app",
+  storageBucket: "pantry-assistant-app.appspot.com",
+  messagingSenderId: "950423514077",
+  appId: "1:950423514077:web:8e112629e1564f6a226434"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export { firestore };
